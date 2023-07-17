d069d3376b9493e3b0e2a6127a9b3cfdfd8faa6e
Daniel Castillo <danielcastillo89@icloud.com>
2023-07-17 17:41:47 -0500

    project router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
 // Write your "projects" router here!
+const express = require("express");
+const Project = require("./projects-model");
+const router = express.Router();
+
+router.use(express.json());
+
+router.get("/", (req, res) => {
+  Project.get()
+    .then((projects) => {
+      if (projects) {
+        res.status(200).json(projects);
+      } else {
+        res.status(200).json([]);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error occured while fetching projects",
+      });
+    });
+});
